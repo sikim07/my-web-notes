3b3a6b37cc4d27e3625f2f42463a3ea70ad69e2e
sikim07 <skim88.1942@gmail.com>
2023-11-20 22:24:32 +0900

    Feat: HomeMain 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import LayoutMain from '@/components/layouts/LayoutMain'
+import HomeMain from '@/components/home/HomeMain'
 
 const Home = () => {
-    return <LayoutMain>메인 페이지</LayoutMain>
+    return (
+        <LayoutMain>
+            <HomeMain />
+        </LayoutMain>
+    )
 }
 
 export default Home
