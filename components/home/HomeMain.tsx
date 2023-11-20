28ae6375140c0683dbd8aac68a9bc758fa2afbd9
sikim07 <skim88.1942@gmail.com>
2023-11-21 08:47:17 +0900

    Fix: count 로직 수정(useRef 활용)

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 const HomeMain = (): JSX.Element => {
     const words = '12345'
@@ -8,23 +8,21 @@ const HomeMain = (): JSX.Element => {
     const [writtenWords, setWrittenWords] = useState('')
     const [proceedingWord, setProceedingWord] = useState('')
     const [scheduledWord, setScheduledWord] = useState(words)
+    const countRef = useRef(0)
+
+    useMemo(() => {
+        const textingInterval: NodeJS.Timeout = setInterval(() => {
+            countRef.current++
+            setCount(countRef.current)
+            if (countRef.current > maxCount) clearInterval(textingInterval)
+        }, 1000)
+    }, [])
 
     useMemo(() => {
         setProceedingWord(words.at(count) ?? '')
         setWrittenWords((current) => current + proceedingWord)
         setScheduledWord(words.substring(count + 1))
-    }, [count])
-
-    useMemo(() => {
-        const textingInterval = setInterval(() => {
-            setCount((current) => (current += 1))
-        }, 1000)
-
-        if (count > maxCount) console.log('end')
-        return () => {
-            clearInterval(textingInterval)
-        }
-    }, [])
+    }, [countRef.current])
 
     return (
         <div>
