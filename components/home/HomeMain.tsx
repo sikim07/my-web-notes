104a5e9491294310d4cea3e9137ad1d02db599d4
sikim07 <skim88.1942@gmail.com>
2023-12-03 17:37:41 +0900

    Fix: 불필요 컨텐츠 제거

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -1,37 +1,5 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-
 const HomeMain = (): JSX.Element => {
-    const words = '12345'
-    const maxCount = words.length - 1
-
-    const [count, setCount] = useState(0)
-    const [writtenWords, setWrittenWords] = useState('')
-    const [proceedingWord, setProceedingWord] = useState('')
-    const [scheduledWord, setScheduledWord] = useState(words)
-    const countRef = useRef(0)
-
-    useMemo(() => {
-        const textingInterval: NodeJS.Timeout = setInterval(() => {
-            countRef.current++
-            setCount(countRef.current)
-            if (countRef.current > maxCount) clearInterval(textingInterval)
-        }, 1000)
-    }, [])
-
-    useMemo(() => {
-        setProceedingWord(words.at(count) ?? '')
-        setWrittenWords((current) => current + proceedingWord)
-        setScheduledWord(words.substring(count + 1))
-    }, [countRef.current])
-
-    return (
-        <div>
-            <span className="text-[#333]">{writtenWords}</span>
-            <span className="bg-black text-[#fff]">{proceedingWord}</span>
-            <span className="text-[#333]">{count <= maxCount && '_'}</span>
-            <span className="hidden">{scheduledWord}</span>
-        </div>
-    )
+    return <div>Main Home</div>
 }
 
 export default HomeMain
