954121391b4a6b59803229302fb2b35f13dfe0ce
sikim07 <skim88.1942@gmail.com>
2023-12-03 18:34:40 +0900

    feat: call test list api

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react'
+
 const HomeMain = (): JSX.Element => {
+    const [test, setTest] = useState()
+    useEffect(() => {
+        const fetchData = async () => {
+            const results = await fetch('/api/list').then((response) => response.json())
+            setTest(results)
+        }
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        console.log('test', test)
+    }, [test])
+
     return <div>Main Home</div>
 }
 
