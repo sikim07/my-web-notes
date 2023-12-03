888e04cf9c2f3426b5dc039d026e20b3f34381b3
sikim07 <skim88.1942@gmail.com>
2023-12-03 18:33:35 +0900

    Feat: mongodb 환경변수 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        DB_CONNECT_URI: process.env.DB_CONNECT_URI,
+        DB_TEST_COLLECTION: process.env.DB_TEST_COLLECTION,
+    },
 }
 
 module.exports = nextConfig
