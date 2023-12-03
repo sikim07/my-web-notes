25f1e71fe2a2372849f31dbc91006a7fc5cc67a8
sikim07 <skim88.1942@gmail.com>
2023-12-03 19:17:00 +0900

    Feat: add exceptions

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,23 +1,17 @@
 import { MongoClient, Db } from 'mongodb'
-
-const uri = process.env.DB_CONNECT_URI
-
-let mongoClient: MongoClient | null = null
-let database: Db | null = null
-
-if (!process.env.DB_CONNECT_URI) {
-    throw new Error('Please add your Mongo URI to .env.local')
-}
+import Exception, { CF_ERROR_CODES } from '@/configs/error'
 
 export async function connectToDatabase() {
-    try {
-        if (!uri) return {}
-        if (mongoClient && database) {
-            return { mongoClient, database }
-        }
+    const uri: string | undefined = process.env.DB_CONNECT_URI
+    if (!uri) {
+        throw new Exception(CF_ERROR_CODES.ENV_UNDEFINED)
+    }
 
-        mongoClient = await new MongoClient(uri).connect()
-        database = await mongoClient.db('Test')
+    try {
+        if (uri === undefined) throw new Exception(CF_ERROR_CODES.ENV_UNDEFINED)
+
+        const mongoClient: MongoClient | null = await new MongoClient(uri).connect()
+        const database: Db | null = mongoClient.db('Test')
         return { mongoClient, database }
     } catch (e) {
         console.error(e)
---
+++ pages/api/test_list.ts
@@ -0,0 +1,23 @@
+import Exception, { CF_ERROR_CODES } from '@/configs/error'
+import { connectToDatabase } from '@/lib/mongodb'
+import { Db } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next/types'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+    const connection = await connectToDatabase()
+    const database: Db | undefined = connection?.database
+    if (database === undefined) throw new Exception(CF_ERROR_CODES.DB_CONNECTION_UNDFINED)
+
+    const collection = database.collection('Test1')
+
+    if (request.method === 'GET') {
+        const results = await collection.find({}).limit(10).toArray()
+        response.status(200).json(results)
+    } else if (request.method === 'POST') {
+        const condition: string | string[] | undefined = request.query.id
+        if (condition === undefined) throw new Exception(CF_ERROR_CODES.NO_QUERY_PARAMS)
+        const data = request.body
+    }
+}
+
+export default handler
