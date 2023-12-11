544e81d8cc793bfa8853ab866cbbc52f17116124
sikim07 <skim88.1942@gmail.com>
2023-12-11 09:28:42 +0900

    Feat: navigation 링크 추가

--- components/layouts/HeaderMain.tsx
+++ components/layouts/HeaderMain.tsx
@@ -1,10 +1,31 @@
+import Link from 'next/link'
 import DarkModeSwitch from '../buttons/DarkModeSwitch'
+
+const NAVIGATE_MENU = [
+    { id: 0, title: '1', url: '/' },
+    {
+        id: 1,
+        title: '2',
+        url: '/dialog',
+    },
+    {
+        id: 2,
+        title: '3',
+        url: '/',
+    },
+]
 
 const HeaderMain = (): JSX.Element => {
     return (
         <header className="flex h-[80px] items-center justify-between px-[20px]">
             <div className="">로고</div>
-            <nav className="">1 | 2 | 3</nav>
+            <nav className="flex gap-[10px]">
+                {NAVIGATE_MENU.map((menu) => (
+                    <div key={menu.id}>
+                        <Link href={menu.url}>{menu.title}</Link>
+                    </div>
+                ))}
+            </nav>
             <DarkModeSwitch />
         </header>
     )
